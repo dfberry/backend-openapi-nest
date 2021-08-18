96f44e314d6136c63d199f8ff2ee2de3d5c599c8
shanurrahman <shanur.cse.nitap@gmail.com>
2021-08-18 15:21:03 +0530

    creating a new template repository

---
+++ src/main.ts
@@ -0,0 +1,41 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  // const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({ logger: true }),
+  );
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Delivery Microservice')
+    .setDescription('Delivery microservice built with fastify')
+    .setVersion('1.0')
+    .addTag('delivery')
+    .build();
+
+  const customOptions: SwaggerCustomOptions = {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  };
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, customOptions);
+
+  await app.listen(8080);
+}
+bootstrap();
